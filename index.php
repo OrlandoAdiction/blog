db1b165eeb96a8b8127e95b1ad1d44fd3c6bd52f
Orlando <luis.paucar@Ismem.edu.pe>
2022-04-27 21:59:49 -0500

    Failed1

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
-<link rel="stylesheet" type="text/css" href="estilos.css">
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url');?>/estilos.css">
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,11 +9,11 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
-<body background="stmpddd.jpg">
+<body background="<?php bloginfo('template_url');?>/stmpddd.jpg">
 	<header class="container-flip">
-		<h1 class="texto0">SENTIO</h1>
+		<h1><?php bloginfo('name');?></h1>
 
-		<img src="stmmp.jpg" class="stmp" width="100">
+		<img src="<?php bloginfo('template_url');?>/stmmp.jpg" class="stmp" width="100">
            	<ul class="menu">
            	<li>Follow</li>
            	<li>Limitles</li>
@@ -24,9 +24,18 @@
            	<li>Funk</li>
            	</ul>     
     </header>
-<p></p>
+<section>
+    <?php $articulos= new WP_Query([
+                   'showposts' => 3,
+                  ]);
+while ($articulos->have_posts()){
+    $articulos->the_post();
 
-</div><img src="follow.jpg" class="grafico">
+    the_title();   
+    echo '<hr>';
+}?>
+
+</div><img src="<?php bloginfo('template_url');?>/follow.jpg" class="grafico">
 <h2 class="texto1">FOLLOW</h2>
 <h4 class="texto2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -35,10 +44,10 @@ consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h4>
 
-<p></p>
 
 
-</div><img src="Limitles.jpg" class="grafico" width="500">
+
+</div><img src="<?php bloginfo('template_url');?>/Limitles.jpg" class="grafico" width="500">
 <h2 class="texto1">LIMITLESS</h2>
 <h4 class="texto2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -47,9 +56,9 @@ consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h4>
 
-<p></p>
 
-</div><img src="Reboot.jpg" class="grafico" width="500">
+
+</div><img src="<?php bloginfo('template_url');?>/Reboot.jpg" class="grafico" width="500">
 <h2 class="texto1">REBOOT</h2>
 <h4 class="texto2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -58,10 +67,10 @@ consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h4>
 
-<p></p>
 
 
-</div><img src="Quantum.jpg" class="grafico" width="500">
+
+</div><img src="<?php bloginfo('template_url');?>/Quantum.jpg" class="grafico" width="500">
 <h2 class="texto1">QUANTUM</h2>
 <h4 class="texto2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -71,9 +80,9 @@ cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h4>
 
 
-<p></p>
 
-</div><img src="God.jpg" class="grafico" width="500">
+
+</div><img src="<?php bloginfo('template_url');?>/God.jpg" class="grafico" width="500">
 <h2 class="texto1">GOOD MORNING</h2>
 <h4 class="texto2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -82,9 +91,9 @@ consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h4>
 
-<p></p>
 
-</div><img src="Starlight.jpg" class="grafico" width="500">
+
+</div><img src="<?php bloginfo('template_url');?>/Starlight.jpg" class="grafico" width="500">
 <h2 class="texto" >STARLIGHT</h2>
 <h4 class="texto2">
 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
@@ -94,9 +103,9 @@ consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h4>
 
-<p></p>
 
-</div><img src="Funk.jpg" class="grafico" width="500">
+
+</div><img src="<?php bloginfo('template_url');?>/Funk.jpg" class="grafico" width="500">
 <h2 class="texto" >FUNK</h2>
 <h4 class="texto2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -104,7 +113,10 @@ quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h4>
-
+</section>
+<footer class="container-flip">
+	
+</footer>
 
 
 
