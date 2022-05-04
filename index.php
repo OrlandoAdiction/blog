63a0e27f574862e4311f049091f4d474101fda8c
Orlando <luis.paucar@Ismem.edu.pe>
2022-05-03 21:10:03 -0500

    actualise

--- index.php
+++ index.php
@@ -1,30 +1,5 @@
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
-<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url');?>/estilos.css">
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
-<body background="<?php bloginfo('template_url');?>/stmpddd.jpg">
-	<header class="container-flip">
-		<h1><?php bloginfo('name');?></h1>
-
-		<img src="<?php bloginfo('template_url');?>/stmmp.jpg" class="stmp" width="100">
-           	<ul class="menu">
-           	<li>Follow</li>
-           	<li>Limitles</li>
-           	<li>Reboot</li>
-           	<li>Quantum</li>
-           	<li>Good Morn</li>
-           	<li>Starlight</li> 
-           	<li>Funk</li>
-           	</ul>     
-    </header>
-<section>
+<?php get_header();?>
+<section class="container">
     <?php $articulos= new WP_Query([
                    'showposts' => 3,
                   ]);
@@ -33,14 +8,18 @@ while ($articulos->have_posts()) {
 
 
 
+<div class="row cuadro"> 
+	<div class="col-sm-3"> 
+		<?php the_post_thumbnail("thumbnail");?> 
+	</div>
+	<div class="col-sm-9">
+		<h2 class="tit"><a href="<?php the_permalink() ?>"><?php the_title();?></a></h2>
+		<p><?php the_author();?> el <?php the_time("d/m/Y");?></p> 
 
-<?php the_post_thumbnail("large");?> 
-<h2><?php the_title();?></h2>
-<?php the_excerpt();?>
+        <?php the_excerpt();?>
 
-    
-
-
+	</div>
+</div>
 
 
 
---
+++ single.php
@@ -0,0 +1,16 @@
+</head>
+<body background="<?php bloginfo('template_url');?>/logox2.jpg" width="700">
+	<header class="container-flip">
+		<h1><?php bloginfo('name');?></h1>
+
+		<img src="<?php bloginfo('template_url');?>/stmmp.jpg" class="stmp" width="100">
+           	<ul class="menu">
+           	<li>Follow</li>
+           	<li>Limitles</li>
+           	<li>Reboot</li>
+           	<li>Quantum</li>
+           	<li>Good Morn</li>
+           	<li>Starlight</li> 
+           	<li>Funk</li>
+           	</ul>     
+    </header>
